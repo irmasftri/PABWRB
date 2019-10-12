77e308444a4e75f27beaae0c15bac1a3bc94c8fe
irmasftri <50016976+irmasftri@users.noreply.github.com>
2019-10-12 22:46:52 +0700

    lanjutan praktikum ke-6

---
+++ week-6/latihan/latihan3.php
@@ -0,0 +1,9 @@
+<?php
+    $kata = ["lanirne","aduh","qifuat","toda","anevi","samid","kifuat"];
+    sort($kata);
+    echo "Hasil sorting adalah :<br>";
+    for($i=0;$i<7;$i++){
+        echo $kata[$i];
+        echo "<br>";
+    }
+?>
